3af49588d23eea5d292a1d0e9e28ba25918b6c7a
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-24 19:01:18 +0300

    refactor renderer to abstract class

--- app/Pagination/Renderers/PlainRenderer.php
+++ app/Pagination/Renderers/PlainRenderer.php
@@ -3,30 +3,12 @@
 namespace App\Pagination\Renderers;
 
 use App\Pagination\Meta;
+use App\Pagination\Renderers\RendererAbstract;
 
-class PlainRenderer
+class PlainRenderer extends RendererAbstract
 {
-    protected $meta;
-
-    public function __construct(Meta $meta)
-    {
-        $this->meta = $meta;
-    }
-
     public function render()
     {
-        $lrCount = 2;
-
-        $range = range(1, $this->meta->lastPage);
-
-        $endDifference = max(0, $this->meta->page - ($this->meta->lastPage - $lrCount) + 1);
-
-        $range = array_slice($range, max(1, ($this->meta->page - $lrCount) - $endDifference));
-        $range = array_slice($range, 0, ($lrCount * 2));
-
-        array_unshift($range, 1);
-        $range[] = $this->meta->lastPage();
-
-        return array_unique($range);
+        dump($this->pages());
     }
 }
---
+++ app/Pagination/Renderers/RendererAbstract.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Pagination\Renderers;
+
+use App\Pagination\Meta;
+
+abstract class RendererAbstract
+{
+    protected $meta;
+
+    public function __construct(Meta $meta)
+    {
+        $this->meta = $meta;
+    }
+
+    public function pages()
+    {
+        $lrCount = 2;
+
+        $range = range(1, $this->meta->lastPage);
+
+        $endDifference = max(0, $this->meta->page - ($this->meta->lastPage - $lrCount) + 1);
+
+        $range = array_slice($range, max(1, ($this->meta->page - $lrCount) - $endDifference));
+        $range = array_slice($range, 0, ($lrCount * 2));
+
+        array_unshift($range, 1);
+        $range[] = $this->meta->lastPage();
+
+        return array_unique($range);
+    }
+
+    abstract public function render();
+}
