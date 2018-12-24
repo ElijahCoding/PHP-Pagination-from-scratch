11a5efa53998b53fd02b6c9c6122e0ceb66329df
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-24 18:57:02 +0300

    finished page number algorithm

--- app/Pagination/Renderers/PlainRenderer.php
+++ app/Pagination/Renderers/PlainRenderer.php
@@ -19,12 +19,14 @@ class PlainRenderer
 
         $range = range(1, $this->meta->lastPage);
 
-        $range = array_slice($range, max(1, ($this->meta->page - $lrCount)));
+        $endDifference = max(0, $this->meta->page - ($this->meta->lastPage - $lrCount) + 1);
+
+        $range = array_slice($range, max(1, ($this->meta->page - $lrCount) - $endDifference));
         $range = array_slice($range, 0, ($lrCount * 2));
 
         array_unshift($range, 1);
         $range[] = $this->meta->lastPage();
-        
-        dump($range);
+
+        return array_unique($range);
     }
 }
